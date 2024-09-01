437559d1318fceed49bc1b0f95f8a09c5894db6f
André Dorneles <andre@imperiosoft.com.br>
2024-09-01 14:30:31 -0300

    Implementado parsing JSON e estrutura 'Quotation' em 'BuscaCotacao'
    Adicionado código para realizar o parsing do JSON retornado pela API de cotação e mapeá-lo para a nova estrutura 'Quotation'. Isso permite um tratamento mais robusto e preciso das informações recebidas da API, melhorando a manutenibilidade e clareza do código. Além disso, corrigida a formatação da resposta no handler 'BuscaCotacaoHandler' para refletir a nova estrutura.

--- server.go
+++ server.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
+
+type Quotation struct {
+	Code       string `json:"code"`
+	Codein     string `json:"codein"`
+	Name       string `json:"name"`
+	High       string `json:"high"`
+	Low        string `json:"low"`
+	VarBid     string `json:"varBid"`
+	PctChange  string `json:"pctChange"`
+	Bid        string `json:"bid"`
+	Ask        string `json:"ask"`
+	Timestamp  string `json:"timestamp"`
+	CreateDate string `json:"create_date"`
+}
+
+type QuotationResponse struct {
+	USD_BRL Quotation `json:"USDBRL"`
+}
 
 func main() {
 	http.HandleFunc("/", HelloServerHandler)
@@ -33,18 +53,27 @@ func BuscaCotacaoHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf(`{"cotacao": %f}`, cotacao)))
+	w.Write([]byte(fmt.Sprintf(`{"cotacao": %s}`, cotacao.USD_BRL.Bid)))
 }
 
-func BuscaCotacao() (float32, error) {
+func BuscaCotacao() (*QuotationResponse, error) {
 	url := "https://economia.awesomeapi.com.br/json/last/USD-BRL"
 	resp, err := http.Get(url)
 	if err != nil {
-		return 0, err
+		return nil, err
 	}
 	defer resp.Body.Close()
 
-	fmt.Println(resp.Body)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
 
-	return 5.5, nil
+	var quotationResponse QuotationResponse
+	err = json.Unmarshal(body, &quotationResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return &quotationResponse, nil
 }
