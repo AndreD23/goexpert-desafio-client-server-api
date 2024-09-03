98c276acc9847538c7aef823a341858cf00f29ac
André Dorneles <andre@imperiosoft.com.br>
2024-09-03 11:18:48 -0300

    Adicionado salvamento de cotação em arquivo
    Implementada a função 'SalvarArquivoCotacao' para salvar a cotação de USD-BRL em um arquivo de texto. Também adicionada a função 'BuscaCotacaoServer' com tratamento de erros. Essas mudanças melhoram a robustez do código ao lidar com erros e permitem o armazenamento persistente da cotação.

--- client.go
+++ client.go
@@ -8,10 +8,20 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 func main() {
+	cotacao, err := BuscaCotacaoServer()
+	if err != nil {
+		log.Fatalf("Error fetching USD-BRL: %v", err)
+	}
+
+	SalvarArquivoCotacao(cotacao)
+}
+
+func BuscaCotacaoServer() (string, error) {
 	ctx := context.Background()
 	ctx, cancel := context.WithTimeout(ctx, 300*time.Millisecond)
 	defer cancel()
@@ -20,7 +30,7 @@ func main() {
 
 	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
 	resp, err := http.DefaultClient.Do(req)
@@ -28,13 +38,13 @@ func main() {
 		if errors.Is(ctx.Err(), context.DeadlineExceeded) {
 			log.Printf("Erro ao buscar cotação: timeout do contexto excedido")
 		}
-		panic(err)
+		return "", err
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
 	var quotationResponse quotation.QuotationResponse
@@ -43,5 +53,19 @@ func main() {
 		panic(err)
 	}
 
-	println(quotationResponse.USD_BRL.Bid)
+	return quotationResponse.USD_BRL.Bid, nil
+}
+
+func SalvarArquivoCotacao(cotacao string) {
+	// Salvar arquivo
+	f, err := os.Create("cotacao.txt")
+	if err != nil {
+		log.Fatalf("Error creating file: %v", err)
+	}
+
+	str := "Dólar: " + cotacao
+	_, err = f.WriteString(str)
+	if err != nil {
+		log.Fatalf("Error writing to file: %v", err)
+	}
 }
